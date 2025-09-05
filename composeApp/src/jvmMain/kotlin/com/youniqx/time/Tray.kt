35f2731cb64c1b901fceef278e10cc165267f1bd
Jonas Hackenberg <hackenberg@youniqx.com>
2025-09-05 08:59:01 +0200

    feat: add desktop tray handling

---
+++ composeApp/src/jvmMain/kotlin/com/youniqx/time/Tray.kt
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.youniqx.time
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCompositionContext
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toAwtImage
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.window.ApplicationScope
+import androidx.compose.ui.window.MenuScope
+import androidx.compose.ui.window.setContent
+import java.awt.PopupMenu
+import java.awt.SystemTray
+import java.awt.TrayIcon
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
+import java.awt.ComponentOrientation
+import java.awt.GraphicsConfiguration
+import java.awt.GraphicsEnvironment
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+import java.util.Locale
+
+internal enum class DesktopPlatform {
+    Linux,
+    Windows,
+    MacOS,
+    Unknown;
+
+    companion object {
+        /**
+         * Identify OS on which the application is currently running.
+         */
+        val Current: DesktopPlatform by lazy {
+            val name = System.getProperty("os.name")
+            when {
+                name?.startsWith("Linux") == true -> Linux
+                name?.startsWith("Win") == true -> Windows
+                name == "Mac OS X" -> MacOS
+                else -> Unknown
+            }
+        }
+    }
+}
+
+val GraphicsConfiguration.density: Density get() = Density(
+    defaultTransform.scaleX.toFloat(),
+    fontScale = 1f
+)
+
+internal val GlobalDensity get() = GraphicsEnvironment.getLocalGraphicsEnvironment()
+    .defaultScreenDevice
+    .defaultConfiguration
+    .density
+
+internal val ComponentOrientation.layoutDirection: LayoutDirection
+    get() = when {
+        isLeftToRight -> LayoutDirection.Ltr
+        isHorizontal -> LayoutDirection.Rtl
+        else -> LayoutDirection.Ltr
+    }
+
+internal val Locale.layoutDirection: LayoutDirection
+    get() = ComponentOrientation.getOrientation(this).layoutDirection
+
+internal val GlobalLayoutDirection get() = Locale.getDefault().layoutDirection
+
+// In fact, this size doesn't affect anything on Windows/Linux, because they request what they
+// need, and not what we provide. It only affects macOs. This size will be scaled in asAwtImage to
+// support DPI=2.0
+// Unfortunately I hadn't enough time to find sources from the official docs
+private val iconSize = when (DesktopPlatform.Current) {
+    // https://doc.qt.io/qt-5/qtwidgets-desktop-systray-example.html (search 22x22)
+    DesktopPlatform.Linux -> Size(22f, 22f)
+    // https://doc.qt.io/qt-5/qtwidgets-desktop-systray-example.html (search 16x16)
+    DesktopPlatform.Windows -> Size(16f, 16f)
+    // https://medium.com/@acwrightdesign/creating-a-macos-menu-bar-application-using-swiftui-54572a5d5f87
+    DesktopPlatform.MacOS -> Size(22f, 22f)
+    DesktopPlatform.Unknown -> Size(32f, 32f)
+}
+
+/**
+ * `true` if the platform supports tray icons in the taskbar
+ */
+val isTraySupported: Boolean get() = SystemTray.isSupported()
+
+// TODO(demin): add mouse click/double-click/right click listeners (can we use PointerInputEvent?)
+/**
+ * Adds tray icon to the platform taskbar if it is supported.
+ *
+ * If tray icon isn't supported by the platform, in the "standard" error output stream
+ * will be printed an error.
+ *
+ * See [isTraySupported] to know if tray icon is supported
+ * (for example to show/hide an option in the application settings)
+ *
+ * @param icon Icon of the tray
+ * @param state State to control tray and show notifications
+ * @param tooltip Hint/tooltip that will be shown to the user
+ * @param menu Context menu of the tray that will be shown to the user on the mouse click (right
+ * click on Windows, left click on macOs).
+ * If it doesn't contain any items then context menu will not be shown.
+ * @param onAction Action performed when user clicks on the tray icon (double click on Windows,
+ * right click on macOs)
+ */
+@Suppress("unused")
+@Composable
+fun ApplicationScope.Tray(
+    icon: Painter,
+    tooltip: String? = null,
+    onClick: (x: Int, y: Int) -> Unit = {_, _ -> },
+    onAction: () -> Unit = {},
+    menu: @Composable MenuScope.() -> Unit = {}
+) {
+    if (!isTraySupported) {
+        DisposableEffect(Unit) {
+            // We should notify developer, but shouldn't throw an exception.
+            // If we would throw an exception, some application wouldn't work on some platforms at
+            // all, if developer doesn't check that application crashes.
+            //
+            // We can do this because we don't return anything in Tray function, and following
+            // code doesn't depend on something that is created/calculated in this function.
+            System.err.println(
+                "Tray is not supported on the current platform. " +
+                        "Use the global property `isTraySupported` to check."
+            )
+            onDispose {}
+        }
+        return
+    }
+
+    val currentOnAction by rememberUpdatedState(onAction)
+
+    val awtIcon = remember(icon) {
+        // We shouldn't use LocalDensity here because Tray's density doesn't equal it. It
+        // equals to the density of the screen on which it shows. Currently Swing doesn't
+        // provide us such information, it only requests an image with the desired width/height
+        // (see MultiResolutionImage.getResolutionVariant). Resources like svg/xml should look okay
+        // because they don't use absolute '.dp' values to draw, they use values which are
+        // relative to their viewport.
+        icon.toAwtImage(GlobalDensity, GlobalLayoutDirection, iconSize)
+    }
+
+    val tray = remember {
+        TrayIcon(awtIcon).apply {
+            isImageAutoSize = true
+            addMouseListener(object : MouseListener {
+                override fun mouseClicked(p0: MouseEvent?) {
+                    println("mouseClicked")
+                }
+
+                override fun mousePressed(p0: MouseEvent?) {
+                    println("mousePressed")
+                }
+
+                override fun mouseReleased(p0: MouseEvent?) {
+                    p0?.let { onClick(it.x, it.y) }
+                }
+
+                override fun mouseEntered(p0: MouseEvent?) {
+                    println("mouseEntered")
+                }
+
+                override fun mouseExited(p0: MouseEvent?) {
+                    println("mouseExited")
+                }
+
+            })
+            addActionListener {
+                currentOnAction()
+            }
+        }
+    }
+    val popupMenu = remember { PopupMenu() }
+    val currentMenu by rememberUpdatedState(menu)
+
+    SideEffect {
+        if (tray.image != awtIcon) tray.image = awtIcon
+        if (tray.toolTip != tooltip) tray.toolTip = tooltip
+    }
+
+    val composition = rememberCompositionContext()
+
+    DisposableEffect(Unit) {
+        tray.popupMenu = popupMenu
+
+        val menuComposition = popupMenu.setContent(composition) {
+            currentMenu()
+        }
+
+        SystemTray.getSystemTray().add(tray)
+
+        onDispose {
+            menuComposition.dispose()
+            SystemTray.getSystemTray().remove(tray)
+        }
+    }
+}
--- composeApp/src/jvmMain/kotlin/com/youniqx/time/main.kt
+++ composeApp/src/jvmMain/kotlin/com/youniqx/time/main.kt
@@ -1,13 +1,95 @@
 package com.youniqx.time
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.delay
+import java.awt.Desktop
+import java.awt.event.WindowEvent
+import java.awt.event.WindowFocusListener
+import kotlin.time.Duration.Companion.seconds
 
 fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Time",
-    ) {
-        App()
+    var count by remember { mutableStateOf(0) }
+    var isOpen by remember { mutableStateOf(true) }
+    var isVisible by remember { mutableStateOf(false) }
+    val windowState = rememberWindowState(
+        position = WindowPosition(0.dp, 0.dp),
+        size = DpSize(400.dp, 800.dp)
+    )
+    val density = LocalDensity.current
+    if (isOpen) {
+        Tray(
+            icon = TrayIcon,
+            onClick = { x, y ->
+                with(density) {
+                    println("$x $y")
+                    windowState.position = WindowPosition(x.toDp() * 2 - windowState.size.width / 2, 45.dp)
+                }
+                isVisible = true
+            }
+        )
+        Window(
+            onCloseRequest = {
+                isOpen = false
+            },
+            state = windowState,
+            visible = isVisible,
+            alwaysOnTop = true,
+            icon = TrayIcon, // Todo
+        ) {
+            LaunchedEffect(isVisible) {
+                if (isVisible) Desktop.getDesktop().requestForeground(true)
+            }
+            DisposableEffect(window) {
+                val listener = object : WindowFocusListener {
+                    override fun windowGainedFocus(p0: WindowEvent?) {
+                    }
+
+                    override fun windowLostFocus(p0: WindowEvent?) {
+                        isVisible = false
+                    }
+
+                }
+                window.addWindowFocusListener(listener)
+                onDispose {
+                    window.removeWindowFocusListener(listener)
+                }
+            }
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Value: $count")
+            }
+        }
+    }
+}
+
+object TrayIcon : Painter() {
+    override val intrinsicSize = Size(256f, 256f)
+
+    override fun DrawScope.onDraw() {
+        drawOval(Color(0xFFBC8F8F))
     }
 }
