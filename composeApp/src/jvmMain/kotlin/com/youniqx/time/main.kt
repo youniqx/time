2f5d6f624fa02a8316761cfb0f6ab4a0083e7048
Salomon Bernhard <salomon@youniqx.com>
2025-11-11 08:28:12 +0100

    test: comment tray usage if is not supported

--- composeApp/src/jvmMain/kotlin/com/youniqx/time/main.kt
+++ composeApp/src/jvmMain/kotlin/com/youniqx/time/main.kt
@@ -26,81 +26,93 @@ import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import java.awt.Desktop
+import java.awt.SystemTray
 import java.awt.event.WindowEvent
 import java.awt.event.WindowFocusListener
 import java.lang.Exception
 
 val isMacOs = System.getProperty("os.name") == "Mac OS X"
 
-fun main() = application {
-    var isOpen by remember { mutableStateOf(true) }
-    var isVisible by remember { mutableStateOf(false) }
-    val windowState = rememberWindowState(
-        position = WindowPosition(0.dp, 0.dp),
-        size = DpSize(400.dp, 800.dp)
-    )
-    val density = LocalDensity.current
-    if (isOpen) {
-        Tray(
-            icon = TrayIcon,
-            onClick = { x, y ->
-                with(density) {
-                    // Todo: check if we need the * 2 only on macOS
-                    windowState.position = WindowPosition(x.toDp() * 2 - windowState.size.width / 2, 45.dp)
-                }
-                isVisible = !isVisible
+fun main() =
+    application {
+        LaunchedEffect(true) {
+            println("--------- WE ARE RUNNING ------------")
+        }
+        var isOpen by remember { mutableStateOf(true) }
+        var isVisible by remember { mutableStateOf(true) }
+        val windowState =
+            rememberWindowState(
+                position = WindowPosition(0.dp, 0.dp),
+                size = DpSize(400.dp, 800.dp),
+            )
+        val density = LocalDensity.current
+        if (isOpen) {
+            if (SystemTray.isSupported()) {
+                Tray(
+                    icon = TrayIcon,
+                    onClick = { x, y ->
+                        with(density) {
+                            // Todo: check if we need the * 2 only on macOS
+                            windowState.position = WindowPosition(x.toDp() * 2 - windowState.size.width / 2, 45.dp)
+                        }
+                        isVisible = !isVisible
+                    },
+                )
             }
-        )
-        val focusRequester = remember { FocusRequester() }
-        Window(
-            onCloseRequest = {
-                isOpen = false
-            },
-            onPreviewKeyEvent = {
-                if (
-                    !it.isMetaPressed &&
-                    !it.isAltPressed &&
-                    !it.isCtrlPressed &&
-                    !it.isShiftPressed &&
-                    it.type == KeyEventType.KeyDown &&
-                    !it.utf16CodePoint.toChar().isISOControl()
+            val focusRequester = remember { FocusRequester() }
+            Window(
+                onCloseRequest = {
+                    isOpen = false
+                },
+                onPreviewKeyEvent = {
+                    if (
+                        !it.isMetaPressed &&
+                        !it.isAltPressed &&
+                        !it.isCtrlPressed &&
+                        !it.isShiftPressed &&
+                        it.type == KeyEventType.KeyDown &&
+                        !it.utf16CodePoint.toChar().isISOControl()
                     ) {
-                    focusRequester.requestFocus()
-                }
-                false
-            },
-            state = windowState,
-            visible = isVisible,
-            alwaysOnTop = true,
-            icon = TrayIcon, // Todo
-        ) {
-            if (isMacOs) LaunchedEffect(true) {
-                window.rootPane.putClientProperty("apple.awt.fullWindowContent", true)
-                window.rootPane.putClientProperty("apple.awt.transparentTitleBar", true)
-                window.rootPane.putClientProperty("apple.awt.windowTitleVisible", false)
-            }
-            LaunchedEffect(isVisible) {
-                if (isVisible) Desktop.getDesktop().requestForeground(true)
-            }
-            DisposableEffect(window) {
-                val listener = object : WindowFocusListener {
-                    override fun windowGainedFocus(p0: WindowEvent?) {
+                        focusRequester.requestFocus()
                     }
-
-                    override fun windowLostFocus(p0: WindowEvent?) {
-                        isVisible = false
+                    false
+                },
+                state = windowState,
+                visible = isVisible,
+                alwaysOnTop = true,
+                icon = TrayIcon, // Todo
+            ) {
+                if (isMacOs) {
+                    LaunchedEffect(true) {
+                        window.rootPane.putClientProperty("apple.awt.fullWindowContent", true)
+                        window.rootPane.putClientProperty("apple.awt.transparentTitleBar", true)
+                        window.rootPane.putClientProperty("apple.awt.windowTitleVisible", false)
                     }
+                }
+                if (SystemTray.isSupported()) {
+                    LaunchedEffect(isVisible) {
+                        if (isVisible) Desktop.getDesktop().requestForeground(true)
+                    }
+                    DisposableEffect(window) {
+                        val listener =
+                            object : WindowFocusListener {
+                                override fun windowGainedFocus(p0: WindowEvent?) {
+                                }
 
+                                override fun windowLostFocus(p0: WindowEvent?) {
+                                    isVisible = false
+                                }
+                            }
+                        window.addWindowFocusListener(listener)
+                        onDispose {
+                            window.removeWindowFocusListener(listener)
+                        }
+                    }
                 }
-                window.addWindowFocusListener(listener)
-                onDispose {
-                    window.removeWindowFocusListener(listener)
-                }
+                App(token = System.getenv("youniqxGitlabPackageRegistryToken"), focusRequester = focusRequester)
             }
-            App(token = System.getenv("youniqxGitlabPackageRegistryToken"), focusRequester = focusRequester)
         }
     }
-}
 
 object TrayIcon : Painter() {
     override val intrinsicSize = Size(256f, 256f)
