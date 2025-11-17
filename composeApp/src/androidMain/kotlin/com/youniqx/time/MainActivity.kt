4642790ba976ccbaf1ea135eb20f883e02415948
Jonas Hackenberg <hackenberg@youniqx.com>
2025-11-17 08:30:13 +0100

    fix: remove debug stuff

--- composeApp/src/androidMain/kotlin/com/youniqx/time/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/youniqx/time/MainActivity.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalLayoutApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
