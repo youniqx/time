100ea7873af450d1f5ca55a8316882650065cdb3
Jonas Hackenberg <hackenberg@youniqx.com>
2026-03-11 15:24:29 +0100

    refactor: according to shellcheck

--- scripts/github/authenticate.sh
+++ scripts/github/authenticate.sh
@@ -2,4 +2,6 @@
 
 dir=$(dirname "$0")
 jwt=$("${dir}/generateJwt.sh" "$GITHUB_APP_CLIENT_ID" "$GITHUB_APP_PRIVATE_KEY")
-export GITHUB_TOKEN=$("${dir}/generateInstallationAccessToken.sh" "$GITHUB_INSTALLATION_ID" "$jwt")
+GITHUB_TOKEN=$("${dir}/generateInstallationAccessToken.sh" "$GITHUB_INSTALLATION_ID" "$jwt")
+
+export GITHUB_TOKEN
