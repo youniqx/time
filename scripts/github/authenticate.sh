40b0acf04b8de60eac6e27f92995415d11ce6679
Jonas Hackenberg <hackenberg@youniqx.com>
2026-03-12 14:16:40 +0100

    fix: wrong GITHUB_APP_INSTALLATION_ID variable usage; release command

--- scripts/github/authenticate.sh
+++ scripts/github/authenticate.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
 dir=$(dirname "$0")
-jwt=$("${dir}/generateJwt.sh" "$GITHUB_APP_CLIENT_ID" "$GITHUB_APP_PRIVATE_KEY")
-eval "${dir}/generateInstallationAccessToken.sh ${GITHUB_INSTALLATION_ID} ${jwt}"
+echo "===Generate JWT===" >&2
+jwt=$("${dir}/generateJwt.sh" "$GITHUB_APP_CLIENT_ID" "$GITHUB_APP_PRIVATE_KEY" || exit 1)
+if [[ -z "$jwt" || "$jwt" == "null" ]]; then
+    echo "Error: jwt is not set or is empty." >&2
+    exit 1
+fi
+echo "===Generate IAT===" >&2
+token=$("${dir}/generateInstallationAccessToken.sh" "${GITHUB_APP_INSTALLATION_ID}" "${jwt}" || exit 1)
+if [[ -z "$token" || "$token" == "null" ]]; then
+    echo "Error: token is not set or is empty." >&2
+    exit 1
+fi
+echo "$token"
--- scripts/github/generateInstallationAccessToken.sh
+++ scripts/github/generateInstallationAccessToken.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-response=$(curl --fail --request POST \
+response=$(curl --request POST \
 --url "https://api.github.com/app/installations/$1/access_tokens" \
 --header "Accept: application/vnd.github+json" \
 --header "Authorization: Bearer $2" \
 --header "X-GitHub-Api-Version: 2022-11-28" \
 -d '{"permissions":{"contents":"write"}}')
 
-echo "$response" | jq -r '.token'
+token=$(echo "$response" | jq -r '.token')
 
+echo "$token"
